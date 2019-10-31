d149e3e2fe33e72233f8feed9ec0b627767e217c
Olabanji121 <olabanjiabimbola@gmail.com>
2019-10-31 21:51:16 +0100

    top 30

--- src/Pages/Recipes.js
+++ src/Pages/Recipes.js
@@ -1,36 +1,61 @@
-import React, { Component } from 'react'
-import RecipeList from '../components/RecipeList'
-import Search from '../components/Search';
-import {recipeData} from '../data/tempList';
+import React, { Component } from "react";
+import RecipeList from "../components/RecipeList";
+import Search from "../components/Search";
+import { recipeData } from "../data/tempList";
 
 export default class Recipes extends Component {
-    constructor(props){
-        super(props)
-    }
-    state={
-        recipes: recipeData,
-        search:'',
-    }
+	constructor(props) {
+		super(props);
+		this.getRecipes = this.getRecipes.bind(this);
+	}
 
-    handleChange =(e)=>{
-        this.setState({
-            search: e.target.value,
-        })
-    };
+	state = {
+		recipes: [],
+		search: "",
+		url: `https://www.food2fork.com/api/search?key=${process.env.REACT_APP_API_KEY}`
+	};
 
-    handleSubmit=(e)=>{
-        e.preventDefault();
-        // console.log(this.state)
-    }
+	async getRecipes() {
+		try {
+			const data = await fetch(this.state.url);
+			// console.log(data);
 
+			const jsonData = await data.json();
+			// console.log(jsonData);
 
-    render() {
-        return (
-            <>
-                <Search search={this.state.search} handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
-                <RecipeList recipes={this.state.recipes}/>
-            </>
-            
-        )
-    }
+			this.setState({
+				recipes: jsonData.recipes
+			});
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	componentDidMount() {
+		this.getRecipes();
+	}
+
+	handleChange = e => {
+		this.setState({
+			search: e.target.value
+		});
+	};
+
+	handleSubmit = e => {
+		e.preventDefault();
+		// console.log(this.state)
+	};
+
+	render() {
+		return (
+			<>
+				<Search
+					search={this.state.search}
+					handleChange={this.handleChange}
+					handleSubmit={this.handleSubmit}
+				/>
+				<RecipeList recipes={this.state.recipes} />
+			</>
+		);
+	}
 }
